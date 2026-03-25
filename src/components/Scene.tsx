a0cd488c5331c95c5c419f2c14621be5c81af167
Danny Bellion <dannybellion@gmail.com>
2026-03-25 17:06:06 +0000

    Fix camera orientation after fullscreen toggle

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useEffect } from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
 import { throttleRender } from '@/utils/fpsThrottle';
 import { CAMERA_CONFIG } from '@/utils/constants';
+
+function ResizeHandler() {
+  const { camera, gl } = useThree();
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = gl.domElement.clientWidth;
+      const height = gl.domElement.clientHeight;
+      if (camera.type === 'OrthographicCamera') {
+        const cam = camera as any;
+        cam.left = width / -2;
+        cam.right = width / 2;
+        cam.top = height / 2;
+        cam.bottom = height / -2;
+        cam.updateProjectionMatrix();
+      }
+      gl.setSize(width, height);
+    };
+
+    window.addEventListener('resize', handleResize);
+    // Also handle fullscreen changes
+    document.addEventListener('fullscreenchange', handleResize);
+    document.addEventListener('webkitfullscreenchange', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      document.removeEventListener('fullscreenchange', handleResize);
+      document.removeEventListener('webkitfullscreenchange', handleResize);
+    };
+  }, [camera, gl]);
+
+  return null;
+}
 
 /**
  * Scene wrapper component that provides the Three.js Canvas with
@@ -15,9 +48,11 @@ const Scene = ({ children }) => {
       camera={CAMERA_CONFIG}
       frameloop="always"
       onCreated={throttleRender}
+      resize={{ scroll: false, debounce: { scroll: 0, resize: 100 } }}
     >
       <color attach="background" args={['#87CEEB']} />
       <ambientLight intensity={0.4} />
+      <ResizeHandler />
       {children}
     </Canvas>
   );
